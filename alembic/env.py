3ade0bb6523b432470a1873bd952fa1b706ac720
zifrit <zifrit@list.ru>
2025-03-21 22:29:46 +0300

    fix alembic

--- alembic/env.py
+++ alembic/env.py
@@ -6,7 +6,7 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
-from src.core.db_connections import settings
+from src.core.settings import settings
 from src.models import Base
 
 # this is the Alembic Config object, which provides
