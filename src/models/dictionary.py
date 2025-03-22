8dbec229630c457628c410bc4242ed9d3c5ae204
zifrit <zifrit@list.ru>
2025-03-22 17:37:07 +0300

    added description

--- src/models/dictionary.py
+++ src/models/dictionary.py
@@ -8,14 +8,16 @@ from sqlalchemy import String, ForeignKey, DateTime, Integer, Text, Boolean
 class Dictionary(IdCUDMixin):
     __tablename__ = "dictionary"
 
-    name: Mapped[str] = mapped_column(String(), comment="Название словаря")
+    name: Mapped[str] = mapped_column(String(30), comment="Название словаря")
+    description: Mapped[str | None] = mapped_column(String(150), comment="Описание")
     topics: Mapped[List["Topic"]] = relationship(back_populates="dictionary")
 
 
 class Topic(IdCUDMixin):
     __tablename__ = "topic"
 
-    name: Mapped[str] = mapped_column(String(), comment="Название топика")
+    name: Mapped[str] = mapped_column(String(30), comment="Название топика")
+    description: Mapped[str | None] = mapped_column(String(150), comment="Описание")
     words: Mapped[List["Words"]] = relationship(back_populates="topic")
     dictionary_id: Mapped[int] = mapped_column(ForeignKey("dictionary.id"))
     dictionary: Mapped["Dictionary"] = relationship(back_populates="topics")
